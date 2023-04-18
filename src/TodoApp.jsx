26c80ef654da36a6763d32583b3bb8835ea669b9
Steeve Mbr1 <steeve.mombrun@outlook.com>
2023-04-18 10:35:02 +0200

    Add data persistence on the local storage

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,51 +1,65 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TaskForm from './TaskForm'
 import TodoList from './TodoList'
 import './TodoApp.css'
 
 function TodoApp() {
   
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const list = localStorage.getItem("TodoListApp")
+    return list ? JSON.parse(list) : []
+  })
   
-
+  useEffect(() => {
+    localStorage.setItem("TodoListApp", JSON.stringify(todos))
+  }, [todos])
+  
+  
   function addNewTask(title) {
     if (title !== "")
-      setTodos(current => {
-        return [
-          ...current,
-          {
-            id : crypto.randomUUID(),
-            title : title,
-            isDone : false,
-          }
-        ]
-      })
+    setTodos(current => {
+      return [
+        ...current,
+        {
+          id : crypto.randomUUID(),
+          title : title,
+          isDone : false,
+        }
+      ]
+    })
   }
-
+  
   function toggleIsDone(id, isDone) {
     setTodos(current => {
       const list = current.map(task => {
         if (task.id === id)
-          return {...task, isDone}
+        return {...task, isDone}
         return task
       })
-      return list.sort((a, b) => (a.isDone ? 1 : -1))
+      return list.sort((a, b) => {
+        if (a.isDone === b.isDone)
+          return 0
+        else if (a.isDone)
+          return 1
+        return -1
+      })
     })
   }
-
+  
   function deleteTask(id) {
     setTodos(current => {
       return current.filter(task => task.id !== id)
     })
   }
-
+  
   return (
     <>
       <TaskForm onSubmit={addNewTask}/>
-      <h1 className="header">Todo List</h1>
+        <h1 className="header">Todo List</h1>
       <TodoList todos={todos} toggleIsDone={toggleIsDone} deleteTask={deleteTask} />
     </>
-  )
-}
-
-export default TodoApp
+    )
+  }
+  
+  export default TodoApp
+  
